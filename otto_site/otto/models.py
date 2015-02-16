b2a6430ff9824b41889a0770dc1b22716d712d49
ulmusic <ulmusic@users.noreply.github.com>
2015-02-16 18:17:00 -0500

    removed the testapp dir

--- otto_site/otto/models.py
+++ otto_site/otto/models.py
@@ -17,24 +17,24 @@ class Submission(models.Model):
     comments = ndb.KeyProperty(kind=Comment, repeated=True)
 
 class Assignment(ndb.model):
-  title = ndb.StringProperty()
-  id_str = ndb.StringProperty()
-  due_date = ndb.DateTimeProperty()
-  date_created = ndb.DateTimeProperty(auto_now_add=True)
-  description = ndb.StringProperty()
-  changes = ndb.StringProperty(repeated=true)  ##probably want a list of changes
-  submissions = ndb.StructuredProperty(Submissions, repeated=True)
-  comments = ndb.StructuredProperty(Comment, repeated=True)  ##are we creating comment objects?
+    title = ndb.StringProperty()
+    id_str = ndb.StringProperty()
+    due_date = ndb.DateTimeProperty()
+    date_created = ndb.DateTimeProperty(auto_now_add=True)
+    description = ndb.StringProperty()
+    changes = ndb.StringProperty(repeated=true)  ##probably want a list of changes
+    submissions = ndb.StructuredProperty(Submissions, repeated=True)
+    comments = ndb.StructuredProperty(Comment, repeated=True)  ##are we creating comment objects?
 
 class Course(ndb.model):
-  title = ndb.StringProperty()
-  id_str = ndb.StringProperty()
-  assignments = ndb.StructuredProperty(Assignment, repeated=True)
-  users = ndb.UserProperty(repeated=True)
+    title = ndb.StringProperty()
+    id_str = ndb.StringProperty()
+    assignments = ndb.StructuredProperty(Assignment, repeated=True)
+    users = ndb.UserProperty(repeated=True)
 
 ##in case we want this
 class Comment(ndb.model):
-  date_posted = ndb.DateTimeProperty(auto_now_add=True)
-  message = ndb.StringProperty()
-  poster = ndb.UserProperty()
-  id_str = ndb.StringProperty()
+    date_posted = ndb.DateTimeProperty(auto_now_add=True)
+    message = ndb.StringProperty()
+    poster = ndb.UserProperty()
+    id_str = ndb.StringProperty()
--- otto_site/testapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-from testapp.views import index
-
-urlpatterns = patterns('',
-                (r'^$', index),
-                )
